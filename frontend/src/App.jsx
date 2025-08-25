e4335ac0e9028f103d36585d8303060e3900361d
Saketh Chokkapu <saketh@example.com>
2025-08-25 22:52:55 +0530

    added landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,11 +5,13 @@ import Signin from "./pages/Signin";
 import UpdatePassword from "./pages/UpdatePassword";
 import ForgotPassword from "./pages/ForgotPassword";
 import AuctionHomepage from "./pages/Auctions/AuctionHome";
+import Landing from "./pages/Landing";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Signup />} />
+      <Route path="/" element={<Landing/>} />  {/* 👈 now landing page is root */}
+      <Route path="/signup" element={<Signup />} />
       <Route path="/signin" element={<Signin />} />
       <Route path="/update-password" element={<UpdatePassword />} />
       <Route path="/forgot-password" element={<ForgotPassword />} />
